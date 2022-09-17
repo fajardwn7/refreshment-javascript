0a976b29d6c51faf68558fdd3ad41d7f4dfb8d46
fajar-kargo <muhammad.fajar@mitrais.com>
2022-09-17 10:51:38 +0700

    comment error example

--- src/web-api.ts
+++ src/web-api.ts
@@ -1,2 +1,2 @@
-// error di nodejs
-const request = new XMLHttpRequest();
+// error di nodejs karena tidak ada modul tersebut di NodeJS hanya ada di webbrowser
+// const request = new XMLHttpRequest();
